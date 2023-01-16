d85c69c5813ec7688e35a493bd0517ee612355a4
Vgonma <victorgmassimi.microverse@gmail.com>
2023-01-16 13:17:46 -0600

    part 4:edditing items complete

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,6 +1,6 @@
 /* eslint-disable */
 import React from 'react';
-import TodoList from './TodoList';
+import TodosList from './TodosList';
 import Header from './Header';
 import InputTodo from './InputTodo';
 import { v4 as uuidv4 } from 'uuid';
@@ -33,22 +33,22 @@ class TodoContainer extends React.Component {
           return {
             ...todo,
             completed: !todo.completed,
-          }
+          };
         }
         return todo;
       }),
-    }))
-  }
+    }));
+  };
 
   delTodo = (id) => {
     this.setState({
       todos: [
         ...this.state.todos.filter((todo) => {
           return todo.id !== id;
-        })
-      ]
-    })
-  }
+        }),
+      ],
+    });
+  };
 
   addTodoItem = (title) => {
     const newTodo = {
@@ -58,23 +58,35 @@ class TodoContainer extends React.Component {
     };
     this.setState({
       todos: [...this.state.todos, newTodo],
+    });
+  };
+
+  setUpdate = (updatedTitle, id) => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.title = updatedTitle
+        }
+        return todo
+      }),
     })
   }
-
+  
   render() {
-  return (
-    <div className="container">
-      <div className="inner">
-        <Header />
-        <InputTodo addTodoProps={this.addTodoItem} />
-        <TodoList
-          todos={this.state.todos}
-          handleChangeProps={this.handleChange}
-          deleteTodoProps={this.delTodo}
-        />
+    return (
+      <div className='container'>
+        <div className='inner'>
+          <Header />
+          <InputTodo addTodoProps={this.addTodoItem} />
+          <TodosList
+            todos={this.state.todos}
+            handleChangeProps={this.handleChange}
+            deleteTodoProps={this.delTodo}
+            setUpdate={this.setUpdate}
+          />
+        </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 }
 export default TodoContainer;
