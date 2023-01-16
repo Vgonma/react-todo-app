2eb12bc9b2e379c8380b108dcbcd9db73b84969b
Vgonma <victorgmassimi.microverse@gmail.com>
2023-01-16 12:04:22 -0600

    part2 handling events

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,8 +4,15 @@ class TodoItem extends React.Component {
   render() {
     return (
       <li>
-        <input type="checkbox" checked={this.props.todo.complete} />
+        <input
+        type="checkbox"
+        checked={this.props.todo.completed}
+        onChange={ () => this.props.handleChangeProps(this.props.todo.id)}
+        />
         {this.props.todo.title}
+        <button onClick={() => this.props.delTodoProps(this.props.todo.id)}>
+          Delete
+        </button>
       </li>
     );
   }
