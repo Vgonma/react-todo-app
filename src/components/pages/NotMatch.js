9668abeffef45332094d3be57211dd88992340c4
Vgonma <victorgmassimi.microverse@gmail.com>
2023-01-26 19:17:06 -0600

    Add page not found

--- src/components/pages/NotMatch.js
+++ src/components/pages/NotMatch.js
@@ -1,10 +1,37 @@
 /* eslint-disable */
 import React from "react"
+import { Link } from "react-router-dom";
 
 const NotMatch = () => {
   return (
-    <div>
-      <h3>No match for this page</h3>
+    <div className="about__content">
+      <h1
+      style={{
+        fontSize: "6rem",
+        fontWeight: "600",
+        marginBottom: "2rem",
+        lineHeight: "1em",
+        color: "#ececec",
+        textTransform: "lowercase",
+        textAlign: "center",
+      }}
+    >
+      Page not found
+    </h1>
+    <Link to="/"
+      style={{
+        fontSize: "6rem",
+        fontWeight: "600",
+        marginBottom: "2rem",
+        lineHeight: "1em",
+        color: "#0000ff",
+        textTransform: "lowercase",
+        textAlign: "center",
+        textDecoration: "underline",
+      }}
+    >
+      Take me home
+    </Link>
     </div>
   )
 }
